8140ba2156799f9c4c5208955b4815d87d882d27
nfaillace04 <nfaillace@bowdoin.edu>
2024-10-01 00:16:28 -0400

    Update fixing the meaning check display

---
+++ meaningcheck2.js
@@ -0,0 +1,8 @@
+var meaning_check2 = [
+    {
+    cue: "Please write your own sentence using the following word: foobly"
+    },
+    {
+        cue: "Please write another sentence using the following word: foobly"
+        }
+]
---
+++ meaningcheck3.js
@@ -0,0 +1,8 @@
+var meaning_check3 = [
+{
+    cue: "Please write your own sentence using the following word: nuppical"
+},
+{
+    cue: "Please write another sentence using the following word: nuppical"
+}
+]
