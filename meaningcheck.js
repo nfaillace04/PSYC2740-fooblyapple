adc704372a340dbb32134839f7d10e8a3ad5ec62
ljanas204 <ljanas@bowdoin.edu>
2024-10-21 10:44:32 -0400

    edited meaning check

--- meaningcheck.js
+++ meaningcheck.js
@@ -1,24 +1,22 @@
 var meaning_check1 = [
     {
-        cue: "Please write your own sentence using the following word: dodish"
+        cue: "Please write your own sentence using the following word: <br><br> dodish"
     },
     {
-        cue: "Please write another sentence using the following word: dodish"
+        cue: "Please write your own sentence using the following word: <br><br> foobly"
+    },
+    {
+        cue: "Please write your own sentence using the following word: <br><br> nuppical"
     }
 ]
 var meaning_check2 = [
     {
-    cue: "Please write your own sentence using the following word: foobly"
+        cue: "Please write another sentence using the following word: <br><br> dodish"
     },
     {
-        cue: "Please write another sentence using the following word: foobly"
-        }
-]
-var meaning_check3 = [
-    {
-        cue: "Please write your own sentence using the following word: nuppical"
+        cue: "Please write another sentence using the following word: <br><br> foobly"
     },
     {
-        cue: "Please write another sentence using the following word: nuppical"
+        cue: "Please write another sentence using the following word: <br><br> nuppical"
     }
-    ]
+]
