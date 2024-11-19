c241b60a836d730b81ad7ff86ca43af3eea569c4
ljanas204 <ljanas@bowdoin.edu>
2024-11-19 10:55:05 -0500

    we need to fix apostrophes in sentences

---
+++ Experiment/priming.js
@@ -0,0 +1,542 @@
+var practice_stimuli = [
+    {
+      "block_number": "practice",
+      "triad": 1,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "boff",
+      "target_word": "apple",
+      "correct_response": 1,
+      "image_path": "applehorse2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": "practice",
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "mipp",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": "practice",
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "geck",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": "practice",
+      "triad": 1,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "mipp",
+      "target_word": "apple",
+      "correct_response": 3,
+      "image_path": "horseapple2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": "practice",
+      "triad": 1,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "geck",
+      "target_word": "apple",
+      "correct_response": 1,
+      "image_path": "applehorse2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": "practice",
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "boff",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": "practice",
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "geck",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": "practice",
+      "triad": 1,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "mipp",
+      "target_word": "apple",
+      "correct_response": 3,
+      "image_path": "horseapple2.png",
+      "correct_key": "L"
+    },
+  
+  ]
+  
+  var test_stimuli = [
+  
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "foobly",
+      "target_word": "apple",
+      "correct_response": 1,
+      "image_path": "applehorse2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "mipp",
+      "target_word": "apple",
+      "correct_response": 1,
+      "image_path": "applehorse2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "dodish",
+      "target_word": "apple",
+      "correct_response": 1,
+      "image_path": "applehorse2.png",
+      "correct_key": "A"
+    },
+  
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "nuppical",
+      "target_word": "apple",
+      "correct_response": 1,
+      "image_path": "applehorse2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "boff",
+      "target_word": "apple",
+      "correct_response": 1,
+      "image_path": "applehorse2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "foobly",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "mipp",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "dodish",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+  
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "nuppical",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "boff",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "foobly",
+      "target_word": "apple",
+      "correct_response": 3,
+      "image_path": "horseapple2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "mipp",
+      "target_word": "apple",
+      "correct_response": 3,
+      "image_path": "horseapple2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "dodish",
+      "target_word": "apple",
+      "correct_response": 3,
+      "image_path": "horseapple2.png",
+      "correct_key": "L"
+    },
+
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "nuppical",
+      "target_word": "apple",
+      "correct_response": 3,
+      "image_path": "horseapple2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "boff",
+      "target_word": "apple",
+      "correct_response": 3,
+      "image_path": "horseapple2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "foobly",
+      "target_word": "horse",
+      "correct_response": 1,
+      "image_path": "horseapple2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "mipp",
+      "target_word": "horse",
+      "correct_response": 1,
+      "image_path": "horseapple2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "dodish",
+      "target_word": "horse",
+      "correct_response": 1,
+      "image_path": "horseapple2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "nuppical",
+      "target_word": "horse",
+      "correct_response": 1,
+      "image_path": "horseapple2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "boff",
+      "target_word": "horse",
+      "correct_response": 1,
+      "image_path": "horseapple2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "foobly",
+      "target_word": "apple",
+      "correct_response": 1,
+      "image_path": "applehorse2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "mipp",
+      "target_word": "apple",
+      "correct_response": 1,
+      "image_path": "applehorse2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "dodish",
+      "target_word": "apple",
+      "correct_response": 1,
+      "image_path": "applehorse2.png",
+      "correct_key": "A"
+    },
+
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "nuppical",
+      "target_word": "apple",
+      "correct_response": 1,
+      "image_path": "applehorse2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "boff",
+      "target_word": "apple",
+      "correct_response": 1,
+      "image_path": "applehorse2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "foobly",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "mipp",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "dodish",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+  
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "nuppical",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "apple-horse",
+      "part": "priming",
+      "prime_word": "boff",
+      "target_word": "horse",
+      "correct_response": 3,
+      "image_path": "applehorse2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "foobly",
+      "target_word": "apple",
+      "correct_response": 3,
+      "image_path": "horseapple2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "mipp",
+      "target_word": "apple",
+      "correct_response": 3,
+      "image_path": "horseapple2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "dodish",
+      "target_word": "apple",
+      "correct_response": 3,
+      "image_path": "horseapple2.png",
+      "correct_key": "L"
+    },
+ 
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "nuppical",
+      "target_word": "apple",
+      "correct_response": 3,
+      "image_path": "horseapple2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 1,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "boff",
+      "target_word": "apple",
+      "correct_response": 3,
+      "image_path": "horseapple2.png",
+      "correct_key": "L"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "foobly",
+      "target_word": "horse",
+      "correct_response": 1,
+      "image_path": "horseapple2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "mipp",
+      "target_word": "horse",
+      "correct_response": 1,
+      "image_path": "horseapple2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "dodish",
+      "target_word": "horse",
+      "correct_response": 1,
+      "image_path": "horseapple2.png",
+      "correct_key": "A"
+    },
+
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "nuppical",
+      "target_word": "horse",
+      "correct_response": 1,
+      "image_path": "horseapple2.png",
+      "correct_key": "A"
+    },
+    {
+      "block_number": 1,
+      "triad": 2,
+      "target_image_pair": "horse-apple",
+      "part": "priming",
+      "prime_word": "boff",
+      "target_word": "horse",
+      "correct_response": 1,
+      "image_path": "horseapple2.png",
+      "correct_key": "A"
+    }
+  ]
